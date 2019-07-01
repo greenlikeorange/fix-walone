2e87790181d2065885f3514640f1c39c603c19d4
Thuya Myo Nyunt <ty@kny.co>
2019-07-01 18:11:04 +0630

    Update test case and update readme

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 const fixWalone = require('./main');
 
-const testString = '၁၀ရက်၁၀ လ ၂၀၁၀ နှစ်။ ဝါး ၁၀ချောင်း​ ကို ဝင်္ကမ္ဘာထဲမှာ ဝင်းဝင်းနဲ့ ဝမ်းပြည့်အောင်ဝါး။ ၀၁-၅၀၀၀၀၅ ကိုခေါ် ဝမ်းနည်းပါတယ်လို့ ဝင်္ကဝုတ္တိတွေပြော။';
-const fixed = fixWalone(testString)
+function random() {
+  const oneOrZero = Math.floor(Math.random() * 2);
+  return ['ဝ', '၀'][oneOrZero];
+}
 
-console.log('Test passed:', testString === fixed)
+const testString = '၁၀ရက်၁၀ လ ၂၀၁၀ နှစ်။ ဝါး ၁၀ချောင်း​ ကို ဝင်္ကမ္ဘာထဲမှာ ဝင်းဝင်းနဲ့ ဝမ်းပြည့်အောင်ဝါး။ ၀၁-၅၀၀၀၀၅ ကိုခေါ် ဝမ်းနည်းပါတယ်လို့ ဝင်္ကဝုတ္တိတွေပြော။';
+
+const testCases = new Array(10)
+  .fill(testString)
+  .map(text => text.replace(/၀|ဝ/g, random))
+  .map(fixWalone);
+
+testCases.forEach((t, i) => {
+  console.log(`Test ${i+1}: ${t === testString}`);
+});
